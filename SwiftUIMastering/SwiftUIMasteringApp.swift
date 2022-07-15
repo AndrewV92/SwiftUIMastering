c84a6a2294632156dd6dfff53e4d74f9d2ec71de
AndrewV92 <vorobyevandrew@gmail.com>
2022-07-15 22:12:12 +0300

    blank start

---
+++ SwiftUIMastering/SwiftUIMasteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMasteringApp.swift
+//  SwiftUIMastering
+//
+//  Created by Андрей Воробьев on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMasteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
